d96812b17e4c53a558d370c46401fc7d40315bb3
TaliaMendes <taliafmendes09@gmail.com>
2024-05-06 20:50:31 -0300

    Inicializar projeto

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const connection = require('./db/connection')
 
 const app = express()
 const PORT = 3005
 
+const tutores = []
+
+app.use(express.json())
 
 
+
+//Verificando conexão do banco de dados SQlite
+connection.authenticate()
+.then(() => {
+  console.log('Banco de dados conectado com sucesso')
+})
+.catch((error) => {
+  console.log(error)
+})
+
+//Inicializando o servidor
 app.listen(PORT, () => {
   console.log('Servidor rodando com sucesso!')
 })
