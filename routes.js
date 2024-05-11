eaed36fe616189da27b317677a6f0a1fb015565b
TaliaMendes <taliafmendes09@gmail.com>
2024-05-11 13:33:27 -0300

    feature: Adicionar validação nas operações

--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
-const express     = require('express');
-const router = express.Router();
-
-const bodyParser  = require('body-parser');
+const express     = require('express')
+const router      = express.Router()
+const bodyParser  = require('body-parser')
 const Pet         = require('./models/Pet')
-const Tutor       = require('./models/Tutor');
+const Tutor       = require('./models/Tutor')
 
+//rotas tutors
 router.get('/tutors', async (req, res) => {
   const tutors = await Tutor.findAll({raw:true}) //capturando todos os tutores
   res.status(200).send(tutors)
 })
 
 router.post('/tutor', async (req, res) => {
-  const { name, phone, email, date_of_birth, address } = req.body;
+  const { name, phone, email, date_of_birth, address } = req.body
   
   if(!name || !phone || !email || !date_of_birth || !address){ //Verificar passagem de dados completa
     return res.status(400).json({ error: 'Todos os campos devem ser preenchidos.' })
@@ -43,9 +43,9 @@ router.put('/tutor/:id', async (req, res) => {
   } 
 
   // Verificar se o tutor existe 
-  const tutor = await Tutor.findByPk(id); 
+  const tutor = await Tutor.findByPk(id) 
   if (!tutor) {
-    return res.status(404).json({ error: 'Tutor não encontrado' });
+    return res.status(404).json({ error: 'Tutor não encontrado' })
   }
 
   //Se o tutor existir realiza a atualização
@@ -59,16 +59,16 @@ router.put('/tutor/:id', async (req, res) => {
     })
 })
 
-router.delete('/tutor/:id', async (req, res) => {
-  const tutorid = req.params.id 
+router.delete('/tutor/:tutorid', async (req, res) => {
+  const tutorid = req.params.tutorid 
 
   //Verificar se o tutor existe para remover
-  const tutor = await Tutor.findByPk(tutorid); 
+  const tutor = await Tutor.findByPk(tutorid) 
   if (!tutor) {
-    return res.status(404).json({ error: 'Tutor não encontrado' });
+    return res.status(404).json({ error: 'Tutor não encontrado' })
   }
 
-  await Tutor.destroy({ where: {id : id}})
+  await Tutor.destroy({ where: {id : tutorid}})
   .then(() => {
     res.status(200).json({message: 'Tutor foi removido com sucesso!'})
   })
@@ -78,7 +78,7 @@ router.delete('/tutor/:id', async (req, res) => {
   })
 })
 
-//rotas pet s
+//rotas pets
 router.get('/pets', async (req, res) => {
   const pets = await Pet.findAll({raw:true}) //capturando todos os tutores
   res.status(200).send(pets)
@@ -89,9 +89,9 @@ router.post('/pet/:tutorid', async (req, res) => {
   const { name, species , weight, date_of_birth} = req.body
 
   //Verificar se o tutor existe para adicionar um pet
-  const tutor = await Tutor.findByPk(tutorid); 
+  const tutor = await Tutor.findByPk(tutorid) 
   if (!tutor) {
-    return res.status(404).json({ error: 'Tutor não encontrado para registro de um novo pet' });
+    return res.status(404).json({ error: 'Tutor não encontrado para registro de um novo pet' })
   }
 
   //Criar novo pet e adicionar o tutor
@@ -118,9 +118,9 @@ router.put('/pet/:id/tutor/:tutorid', async (req, res) => {
   }
   
   // Verificar se o pet existe
-  const pet = await Pet.findByPk(id); 
+  const pet = await Pet.findByPk(id) 
   if (!pet) {
-    return res.status(404).json({ error: 'Pet não encontrado' });
+    return res.status(404).json({ error: 'Pet não encontrado' })
   }
 
   await Pet.update(updatePEt, {where : {id : id}, returning: true})
@@ -133,31 +133,29 @@ router.put('/pet/:id/tutor/:tutorid', async (req, res) => {
   })
 })
 
-router.delete('/pet/:petid/tutor/:tutorid', async (req, res) => {
+router.delete('/pet/:petid/tutor/:tutorId', async (req, res) => {
   const id = req.params.petid
-  const tutorid = req.params.tutorid
+  const tutorId = req.params.tutorId
 
   // Verificar se o pet existe
-  const pet = await Pet.findByPk(id); 
+  const pet = await Pet.findByPk(id)
   if (!pet) {
-     return res.status(404).json({ error: 'Pet não encontrado' });
+    return res.status(404).json({ error: 'Pet não encontrado' })
   }
 
-  //Verificar se o pet pertence ao tutor referenciado e realizar a remoção
-  if(pet && Pet.tutorId === tutorid){
-    await Pet.destroy({where: {id : id}})
-    .then(() => {
-      res.status(200).json({message: 'Pet removido com sucesso!'})
-    })
-    .catch((error) => {
-      console.log(error)
-      res.status(500).json(error)
-    })
-  }
-  else{
+  // Verificar se o pet pertence ao tutor referenciado e realizar a remoção
+  if (String(pet.tutorId) === tutorId) {  //foi necessário converter o dado numérico -> string para realizar a comparação
+    await Pet.destroy({ where: { id: id } })
+      .then(() => {
+        res.status(200).json({ message: 'Pet removido com sucesso!' })
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(500).json(error)
+      })
+  } else {
     return res.status(404).json({ error: 'Este Pet não pertence ao tutor referenciado' })
   }
 })
-
 
 module.exports = router;
